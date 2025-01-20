158a8df6b43823d24ccfac80025408cb414f2cba
Ahui <ahuijr@gmail.com>
2025-01-20 17:10:16 -0600

    contact bttn, font demo

--- src/components/placeholder.tsx
+++ src/components/placeholder.tsx
@@ -8,10 +8,7 @@ const Placeholder: React.FC = () => {
   };
 
   return (
-    <div
-      className="relative w-full h-screen overflow-hidden cursor-pointer"
-      onClick={handleClick}
-    >
+    <div className="relative w-full h-screen overflow-hidden">
       <Image
         src="/placeholderWide.png"
         alt="Placeholder"
@@ -20,6 +17,12 @@ const Placeholder: React.FC = () => {
         className="object-cover md:object-center"
         priority
       />
+      <button
+        onClick={handleClick}
+        className="absolute top-[85%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 py-4 px-10 text-black bg-white font-[ObviaNarrowBold] text-2xl hover:scale-110 focus:outline-none transition-transform duration-200 ease-in-out rounded-xl border-4 border-solid border-[#B01E29]"
+        >
+        CONTACTO
+      </button>
     </div>
   );
 };
