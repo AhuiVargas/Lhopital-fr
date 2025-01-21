28dd9ffc01403d2cf10e9fec91c78422b9eafdc2
Ahui <ahuijr@gmail.com>
2025-01-20 18:33:45 -0600

    fx: contact -> contacto, mobile res

--- src/components/placeholder.tsx
+++ src/components/placeholder.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Placeholder: React.FC = () => {
   const handleClick = () => {
-    window.location.href = 'mailto:contact@lhopital-fr.mx';
+    window.location.href = 'mailto:contacto@lhopital-fr.mx';
   };
 
   return (
@@ -13,14 +13,14 @@ const Placeholder: React.FC = () => {
         src="/placeholderWide.png"
         alt="Placeholder"
         fill
-        sizes="100vw, 100vh"
-        className="object-cover md:object-center"
+        sizes="100vw"
+        className="object-cover md:object-center sm:object-top w-full h-full"
         priority
       />
       <button
         onClick={handleClick}
         className="absolute top-[85%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 py-4 px-10 text-black bg-white font-[ObviaNarrowBold] text-2xl hover:scale-110 focus:outline-none transition-transform duration-200 ease-in-out rounded-xl border-4 border-solid border-[#B01E29]"
-        >
+      >
         CONTACTO
       </button>
     </div>
