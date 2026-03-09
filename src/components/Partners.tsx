331143e0f80b3a8dd3c4f99125984159830ea56a
Ahui <ahuijr@gmail.com>
2026-03-09 09:10:26 -0500

    feat: increase Res-Q-Jack logo size

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -13,7 +13,7 @@ const partners = [
 		name: "Res-Q-Jack",
 		src: "/partners/resqjay-logo.webp",
 		href: "/rescate",
-		containerClass: "relative w-44 h-20",
+		containerClass: "relative w-56 h-24",
 	},
 	{
 		name: "PGI",
